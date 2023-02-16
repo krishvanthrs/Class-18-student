565c3245bc98f934c82e86535314e0a43924ab04
Krishvanth RS <122750632+krishvanthrs@users.noreply.github.com>
2023-02-16 19:47:58 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+ var s1;
+
+ function setup() 
+ {
+   createCanvas(400, 400);
+    s1 = new Student("john",12,6);
+     s1.display() 
+     s2 = new Student("Krishvanth",10,5);
+     s2.display();
+    } 
+    function draw() 
+    { 
+      background(51); 
+    }
